f4c6de22814419447f3fb434e5b9e22061458b4a
Pratham1019 <203171336+Pratham1019@users.noreply.github.com>
2025-05-29 22:15:47 +0530

    completed exercise 6 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let rStr = '';
+    for (let i = 0; i < num; i++) {
+        rStr = rStr.concat(string);
+    }
+
+    return rStr;
 };
 
 // Do not edit below this line

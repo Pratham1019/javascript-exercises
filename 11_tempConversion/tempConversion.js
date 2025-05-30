e02701b3df6164873cd231efc31fc9ff65d9c17e
Pratham1019 <203171336+Pratham1019@users.noreply.github.com>
2025-05-30 18:05:21 +0530

    completed exercise 11 tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+	const celsius = (temp - 32) * (5 / 9);
+	return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+	const fahrenheit = (9 / 5) * temp + 32;
+	return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit
 };

28f2734013853818b7c081d176a1719b1deb9a6b
Pratham1019 <203171336+Pratham1019@users.noreply.github.com>
2025-05-30 16:22:57 +0530

    completed exercise 9 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } 
 
+    const max = Math.max(a, b);
+    const min = Math.min(a, b) - 1;
+    return (max * (max + 1) / 2) - (min * (min + 1) / 2);
 };
 
 // Do not edit below this line
